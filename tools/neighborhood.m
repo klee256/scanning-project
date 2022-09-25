924a5f49c1e028c76af115c92dde8f2807d364f8
klee256 <98336492+klee256@users.noreply.github.com>
2022-09-25 19:58:29 -0400

    Update neighborhood.m

--- tools/neighborhood.m
+++ tools/neighborhood.m
@@ -3,13 +3,38 @@
 % Distance away from points is n
 % Also grabs the related material parameters
 
-function [trainJVArray,trainMatArray] = neighborhood(jvData,materialArray,n,padding)
+function [trainJVArray,trainMatArray] = neighborhood(jvData,materialArray,n,padding,scale,varargin)
 
 dim=sqrt(size(jvData,2)/2);
 
-voltage=jvData(:,1:2:end); voltage=rescale(voltage);
-current=jvData(:,2:2:end); current=current*(-1.0); current=rescale(current);
- 
+if nargin==4
+    % This is the original neighborhood funct, no additional inputs
+    % required
+
+    voltage=jvData(:,1:2:end); voltage=rescale(voltage);
+    current=jvData(:,2:2:end); current=current*(-1.0); current=rescale(current);
+end 
+
+if nargin==5
+    % The scaling for the neighborhood needs to be proportional to the original data
+    % If the number of input arguments equals 5, the program assumes you
+    % want to scale the range (to the original data)
+    % The variable "scale" should be the original clean JV training data
+   
+    trainingVoltage=scale(:,1:2:end); mmV=minmax(trainingVoltage);
+    trainingCurrent=scale(:,2:2:end); trainingCurrent=trainingCurrent*(-1.0); mmJ=minmax(trainingCurrent);
+
+    voltage=jvData(:,1:2:end);
+    current=jvData(:,2:2:end); current=current*(-1.0);
+
+    % Remap equation:
+    % range [a,b], input x, output y
+    % y = a + [(x-min(x))/(max(x)-min(x))]*(b-a)
+
+    voltage=voltage./mmV(2);
+    current=(current-mmJ(1))./(mmJ(2)-mmJ(1));
+end 
+
 jvArray=cell(1,size(voltage,2));
 for jj=1:length(jvArray)
     jvArray{jj}=cat(2,voltage(:,jj),current(:,jj));
